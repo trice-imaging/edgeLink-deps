38a7a8dc02e4b3016e37363da7aa53326b8a6d4a
Jason Dictos <jason@triceimaging.com>
2022-10-06 05:19:47 -0700

    WIP

---
+++ updateGitTree.ps1
@@ -0,0 +1,18 @@
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = 'Stop'
+#Requires -Version 7.2
+
+foreach($port in (Get-ChildItem ports -Directory)) {
+	$versionPath = "./versions/$($port.Name[0])-/$($port.Name).json"
+	$portPath = "$(Resolve-Path $port -Relative)" -replace '\\','/'
+
+	Write-Host "Version path: $($versionPath)`nPort Path: $($portPath)"
+
+	$commit = git rev-parse "HEAD:$($port)"
+
+	$versionObj = (Get-Content $versionPath | ConvertFrom-Json)
+	$versionObj.versions[0].'git-tree' = $commit
+	#Set-Content -Path $versionPath -Value (ConvertTo-Json $versionObj)
+
+	Write-Host "Updated $($versionObj)"
+}
